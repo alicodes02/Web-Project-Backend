6e120c0b8697492ef4eac64a511a71eec861e5dc
Muhammad Ali <se.muhammadali01@gmail.com>
2023-12-24 01:14:29 +0500

    added log in route

--- Routes/UserManagementRoutes.js
+++ Routes/UserManagementRoutes.js
@@ -47,5 +47,42 @@ router.post('/signup', async (req,res) => {
     }
 });
 
+router.post('/signin', async (req,res) => {
+
+    console.log(req.body);
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({email,password});
+
+    if(user) {
+
+        const token = jwt.sign({email}, 'shhhh');
+
+        const response = {
+
+            message: 'Sign In Sucessfull',
+            userId: user._id,
+            userfirstName: user.firstName,
+            userEmail: user.email,
+            token: token,
+        };
+
+        res.status(200).json(response);
+    }
+
+    else {
+
+        const response = {
+
+            message: 'Invalid Email or Password',
+        };
+
+        res.status(404).json(response);
+
+    }
+});
+
 module.exports = router;
 
